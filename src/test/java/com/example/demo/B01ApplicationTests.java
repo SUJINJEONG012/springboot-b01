4af55786cc8e0071d6e94435d86e27fd8ac7fdfc
angela <peekaboo32@naver.com>
2023-08-02 09:52:45 +0900

    로그설정과 데이터베이스연결 테스트코드

--- src/test/java/com/example/demo/B01ApplicationTests.java
+++ src/test/java/com/example/demo/B01ApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootTest
 class B01ApplicationTests {
 
