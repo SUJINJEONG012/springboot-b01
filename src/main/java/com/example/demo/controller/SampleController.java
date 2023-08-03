55b3b7c8aca28972cf73352144a7933316d263da
angela <peekaboo32@naver.com>
2023-08-03 10:21:09 +0900

    list, object 를 addAttribute 이용하여 뷰 화면에 출력하기

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,4 +28,46 @@ public class SampleController {
 		List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
 		model.addAttribute("list",list);
 	}
+	
+	class SampleDto{
+		private String p1,p2,p3;
+
+		public String getP1() {
+			return p1;
+		}
+
+		public String getP2() {
+			return p2;
+		}
+		
+		public String getP3() {
+			return p3;
+		}
+	}
+	
+	@GetMapping("/ex/ex2")
+	public void ex2(Model model) {
+		log.info("/ex/ex2...................");
+		
+		List<String> strList = IntStream.range(1,10)
+				.mapToObj(i-> "Data" + i)
+				.collect(Collectors.toList());
+				
+			model.addAttribute("list", strList);
+				
+		Map<String, String> map = new HashMap<>();
+				map.put("A", "AAAA");
+				map.put("B","BBBB");
+				
+		model.addAttribute("map", map);
+		
+		SampleDto sampleDto = new SampleDto();
+		sampleDto.p1 = "value -- p1";
+		sampleDto.p2 = "value -- p2";
+		sampleDto.p3 = "value -- p3";
+		
+		model.addAttribute("dto", sampleDto);
+				
+	}
+
 }
