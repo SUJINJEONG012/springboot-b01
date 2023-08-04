5b146eb3d2a7a8c518059e104dee961d1d0ed9bd
angela <peekaboo32@naver.com>
2023-08-04 17:16:26 +0900

    Querydsl 설정 라이브러리 추가

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -6,4 +6,6 @@ import com.example.demo.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+	//페이징 처리를 위해 findAll()사용
+	
 }
--- src/test/java/com/example/demo/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/demo/repository/BoardRepositoryTests.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.example.demo.domain.Board;
 
@@ -57,6 +61,21 @@ public class BoardRepositoryTests {
 //		Long bno =1L;
 //		boardRepository.deleteById(bno);
 //	}
+	
+	@Test
+	public void testPaging() {
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+		Page<Board> result = boardRepository.findAll(pageable);
+		
+		log.info("Total count : " + result.getTotalElements());
+		log.info("Total pages : " + result.getTotalElements());
+		log.info("Page number: " + result.getNumber());
+		log.info("Page Size : " + result.getSize());
+		
+		List<Board> todoList = result.getContent();
+		todoList.forEach(board -> log.info(board));
+	
+	}
 	
 	
 	
