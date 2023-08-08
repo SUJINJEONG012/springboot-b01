9de5744640ba8a883bd7bed5aaa823bf57c89241
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-08 21:04:26 +0900

    목록시작페이지/ 끝페이지 등에 대한 처리 계산

--- src/main/java/com/example/demo/dto/PageResponseDto.java
+++ src/main/java/com/example/demo/dto/PageResponseDto.java
@@ -1,5 +1,52 @@
 package com.example.demo.dto;
 
-public class PageResponseDto {
+import java.util.List;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class PageResponseDto<E> {
+	
+	private int page;
+	private int size;
+	private int total;
+	
+	// 시작페이지 번호
+	private int start;
+	// 끝 페이지 번호
+	private int end;
+	// 이전 페이지의 번호
+	private boolean prev;
+	// 다음 페이지의 번호
+	private boolean next;
+	
+	private List<E> dtoList;
+	
+	public PageResponseDto(PageRequestDto pageRequestDto, List<E> dtoList, int total) {
+		if(total > 0) {
+			return;
+		}
+		this.page= pageRequestDto.getPage();
+		this.size= pageRequestDto.getSize();
+		
+		this.total = total;
+		this.dtoList = dtoList;
+		
+		this.end = (int)(Math.ceil(this.page/10.0)) * 10 ;
+		this.start = this.end -9;
+		
+		int last = (int)(Math.ceil((total/(double)size)));
+		
+		this.end = end > last ? last: end;
+		
+		this.prev = this.start > 1;
+		
+		this.next = total > this.end * this.size;
+		
+		
+		
+	}
+	
 }
