dab903139841ff6c7d53c4af57d62746c962f97d
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-03 15:49:41 +0900

    컨트롤러에서 발생하는 예외에 대해 json과 같은 순수한 응답 메세지를 생성해서 보낼 수 있음

--- src/main/java/com/example/demo/controller/advice/CustomRestAdvice.java
+++ src/main/java/com/example/demo/controller/advice/CustomRestAdvice.java
@@ -1,7 +1,42 @@
 package com.example.demo.controller.advice;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.log4j.Log4j2;
 
 // @Valid 과정에서 문제가 발생하면 처리할 수 있도록 advice설계
-public class CustomRestAdvice {
 
+@RestControllerAdvice
+@Log4j2
+public class CustomRestAdvice {
+	
+	@ExceptionHandler(BindException.class)
+	@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+	public ResponseEntity<Map<String, String>> handleBindException(BindException e){
+		
+		log.error(e);
+		
+		Map<String, String> errorMap = new HashMap<>();
+		
+		if(e.hasErrors()) {
+
+			BindingResult bindingResult = e.getBindingResult();
+
+			bindingResult.getFieldErrors().forEach(fieldError -> {
+				errorMap.put(fieldError.getField(), fieldError.getCode());
+			});
+		}
+		
+		return ResponseEntity.badRequest().body(errorMap);
+		
+	}
 }
