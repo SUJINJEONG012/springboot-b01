ea229aa19d7e58d27b630699f3c2b2b85520ce21
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-03 23:34:35 +0900

    특정게시물의 Query 페이징 처리 설정

--- src/main/java/com/example/demo/repository/search/ReplyRepository.java
+++ src/main/java/com/example/demo/repository/search/ReplyRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.domain.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-		
+		@Query("select r from Reply r where r.board.bno = :bno")
+		Page<Reply> listOfBoard(Long bno, Pageable pageable);
 }
